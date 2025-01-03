256ba54f5757be200c3b2b6f13e752828ed4ee26
Andre Wijaya <andrewijaya988@gmail.com>
2025-01-03 21:03:20 +0700

    feat: add sooner to voucher penjual

--- app/voucherPenjual/_components/ConfirmModal.tsx
+++ app/voucherPenjual/_components/ConfirmModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,12 +11,27 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { ReactNode } from "react";
+import { Check, X } from "lucide-react";
 
 interface ConfirmModalProps {
   children: ReactNode; // Menggunakan ReactNode untuk tipe children
 }
 
 export default function ConfirmModal({ children }: ConfirmModalProps) {
+  const submitHandler = () => {
+    return toast("Berhasil Mengakhiri Voucher", {
+      position: "bottom-center",
+      className: "border-green-400 bg-green-100 mb-24",
+      actionButtonStyle: { backgroundColor: "transparent", color: "gray" },
+      action: {
+        label: <X className="h-4 w-4 bg-transparent" />,
+        onClick(event) {
+          return event.cancelable;
+        },
+      },
+      icon: <Check className="text-green-400" />,
+    });
+  };
   return (
     <Dialog>
       <DialogTrigger>{children}</DialogTrigger>
@@ -32,7 +49,10 @@ export default function ConfirmModal({ children }: ConfirmModalProps) {
             </Button>
           </DialogClose>
           <DialogClose className="w-1/2" asChild>
-            <Button className="rounded-full bg-blue-500 text-white  hover:bg-blue-700">
+            <Button
+              className="rounded-full bg-blue-500 text-white  hover:bg-blue-700"
+              onClick={submitHandler}
+            >
               Ya
             </Button>
           </DialogClose>
--- app/voucherPenjual/layout.tsx
+++ app/voucherPenjual/layout.tsx
@@ -1,5 +1,6 @@
 import BottomBar from "@/components/BottomBar";
 import Nav from "./_components/Nav";
+import { Toaster } from "sonner";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
     <section className="max-w-screen-sm min-h-screen mx-auto relative">
       <Nav />
       <div className="px-2">{children}</div>
+      <Toaster />
       <BottomBar />
     </section>
   );
